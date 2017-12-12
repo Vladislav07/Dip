7a429d88f3f684d8795970841a83aea6839c9403
Vladislav <belov_vlad@list.ru>
2017-12-12 20:53:49 +0700

    End

---
+++ SkadTechDomain/Abstract/IitemZsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkadTechDomain.Abstract
+{
+    interface IitemZsRepository
+    {
+        IQueryable<ItemZs>ListItemZs { get; }
+    }
+}
---
+++ Web/Inf/NinjectControllerFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using SkadTechDomain;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using Moq;
+
+namespace Skadtech.Web.Infrastructure
+{
+
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext
+            requestContext, Type controllerType)
+        {
+
+            return controllerType == null
+                ? null
+                : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            
+
+           // ninjectKernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
+        }
+    }
+}
