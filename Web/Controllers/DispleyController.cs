e4a6d0a32d3690736ef3c404b9482fe856cd07b1
Vladislav <belov_vlad@list.ru>
2017-12-12 19:43:59 +0700

    02

---
+++ Web/Controllers/DispleyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkadTechDomain;
+
+namespace Web.Controllers
+{
+    public class DispleyController : Controller
+    {
+        ItemZs itemZs = new ItemZs
+        {
+            IdZs = 1,
+        SubSystem = "BK",
+        ItemZsName="Part01",
+        Type="my",
+        Artikul="Fresh",
+        Esn="00121111"
+        };
+
+    // GET: Displey
+    public ActionResult Index()
+
+        {
+        
+            return View(itemZs);
+        }
+    }
+}
